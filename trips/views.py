db61d67ee62f60cd88acd89a79446b0ddf5c1a86
LeanneShen <maganda.shen@163.com>
2019-10-16 19:22:39 +0800

    INIT

---
+++ trips/views.py
@@ -0,0 +1,15 @@
+# trips/views.py
+
+from django.shortcuts import render
+from .models import Post
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list': post_list,
+    })
